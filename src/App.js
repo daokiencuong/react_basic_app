85bdffc34fbd0587fff7a7ea14cec1ffd3c0b85a
daokiencuong <daokiencuong04@gmail.com>
2025-02-25 18:16:56 +0700

    Learn React state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import MyComponent from './components/MyComponent';
+import logo from "./logo.svg";
+import "./App.css";
+import MyComponent from "./components/MyComponent";
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      Hello World 
-      <MyComponent></MyComponent>
-    </div>
-
-    
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hello world! 
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+class App extends React.Component {
+    state = {
+        name: "John",
+        address: "Hai Ba Trung",
+    };
+    render() {
+        return (
+            <div>
+                Hello World 1243
+                <MyComponent></MyComponent>
+                {
+                    <div>
+                        <p>{this.state.name}</p>
+                        <p>{this.state.address}</p>
+                    </div>
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
