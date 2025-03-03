70dcb2995ce11091442dd86b375deef033929316
daokiencuong <daokiencuong04@gmail.com>
2025-03-03 16:46:24 +0700

    Use React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
 
 const App = () => {
     return (
         <div className="App-container">
-            <Header/>
+            <Header />
+            <div>
+                Test link
+                <div>
+                    <button>
+                        <Link to="/users">Go to user</Link> 
+                    </button>
+                    <button>
+                        <Link to="/admins">Go to admin</Link>
+                    </button>
+                </div>
+            </div>
         </div>
     );
-}
+};
 
 export default App;
