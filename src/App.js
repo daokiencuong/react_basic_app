f5054db2ce341de0e85e4d35113181bc6b8b7517
daokiencuong <daokiencuong04@gmail.com>
2025-03-09 20:25:58 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.scss";
+import {useDispatch, useSelector } from "react-redux";
+import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 
 const App = () => {
+    const dispatch = useDispatch();
+    const count = useSelector(state => state.counter.count);
     return (
         <div className="App-container">
-            Hello World
+            <div>Count = {count}</div>
+        
         </div>
     );
 }
